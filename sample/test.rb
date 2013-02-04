cad9b225d77e43b2184fb4ea146345307244f057
mrsk <ryo5652@gmail.com>
2013-02-04 20:14:37 +0900

    requireするファイルのパスを変更

--- sample/test.rb
+++ sample/test.rb
@@ -1,16 +1,16 @@
 # encoding: utf-8
 #!/usr/bin/env ruby
 
-require File.dirname(__FILE__) + '/../lib/redirect_check.rb'
+require File.expand_path( '../../lib/redirect_check.rb',__FILE__ )
 
 device = {	:android=>'Mozilla/5.0 (Linux; U; Android 4.0.3; ja-jp; ISW13F Build/V51R37G) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30',
 			:basic=>'DoCoMo/2.0 P903i(c100;TB;W24H12)',
 			:iphone=>'Mozilla/5.0 (iPhone; CPU iPhone OS 5_0_1 like Mac OS X) AppleWebKit/534.46 (KHTML, like Gecko) Mobile/9A405 Safari/7534.48.3'
 		}
-		
+
 obj = RedirectCheck.client({
 							:ua=> device[:android], 
-							:file=> File.expand_path( './files/redirect_other_site_4android.txt' )
+							:file=> File.expand_path( '../files/redirect_other_site_4android.txt',__FILE__ )
 							})
 
 res = obj.header
@@ -18,7 +18,7 @@ res = obj.header
 
 obj = RedirectCheck.client({
 							:ua=> device[:basic], 
-							:file=> File.expand_path('./files/redirect_other_site_4basic.txt')
+							:file=> File.expand_path('../files/redirect_other_site_4basic.txt',__FILE__)
 							})
 
 
